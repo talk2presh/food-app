805bd678a0045e3df00eb604aa8eeba31e2e4c52
talk2presh <30320063+talk2presh@users.noreply.github.com>
2022-07-07 13:53:08 +0100

    Add files via upload

---
+++ food-order/admin/manage-admin.php
@@ -0,0 +1,13 @@
+<?php include('partials/menu.php'); ?>
+
+<!--main-content starts -->
+<div class="main-content">
+    <div class="wrapper">
+        <h1>MANAGE-ADMIN</h1>
+
+
+    </div>
+</div>
+<!--main-content ends -->
+
+<?php include('partials/footer.php'); ?>
---
+++ food-order/admin/partials/footer.php
@@ -0,0 +1,10 @@
+    <!--footer starts -->
+    <div class="footer">
+        <div class="wrapper">
+            <p class="text-center">2022 All rights reserved. Developed by Talk2presh</p>
+        </div>
+    </div>
+    <!--footer ends -->
+    </body>
+
+    </html>
